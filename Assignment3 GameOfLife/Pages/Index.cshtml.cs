008c6c6c91e401cf093a6df5136a18c97e3454bd
Tyler Barnes <chalcanthite20@gmail.com>
2018-03-15 18:03:10 -0600

    Spruced some stuff up

--- Assignment3 GameOfLife/Pages/Index.cshtml.cs
+++ Assignment3 GameOfLife/Pages/Index.cshtml.cs
@@ -1,17 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Assignment3_GameOfLife.Pages
 {
-    public class IndexModel : PageModel
+    public class GameModel : PageModel
     {
-        public void OnGet()
-        {
 
-        }
     }
 }
